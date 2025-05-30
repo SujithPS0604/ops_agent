f1d444faddce975de0c7ff6dd67ce33023f03f78
Sujith PS <sujiths@thoughtworks.com>
2025-05-30 15:21:09 +0530

    Add system message

--- ops-agent-backend/mcp-agent.js
+++ ops-agent-backend/mcp-agent.js
@@ -46,8 +46,14 @@ export async function invokeMcpAgent(prompt) {
     // Run the agent with the provided prompt
     return await agent.invoke({
       messages: [
-        // {role: "system", content: "You are a helpful assistant to debug and analyse issues. You are currently in a chat with a user. You are given a prompt and you need to respond to the user's message. You are also given a list of tools that you can use to help you. You can use the tools to get more information about the issue and to help you respond to the user's message. Inorder to analyse a queue, you need to use the tool 'fetchDlqMessagesByQueueName' and then can use tool 'fetchErrorLogs' to get the error logs with the traceId from the dlq."},
-        {role: "user", content: prompt}],
+        {
+          role: "system", content: "You are a helpful assistant to debug and analyse issues. " +
+            "Analyzing a dlq involves : " +
+            "1. Fetching the messages from the dlq using the tool 'fetchDlqMessagesByQueueName' and then use the tool 'fetchErrorLogs' to get the error logs with the traceId from the dlq." +
+            "2. Fetching the order table using the tool 'getOrderTableByOrderId' to get the order table with the orderId." +
+            "3.  Summarise the issue and provide a solution to the user."
+        },
+        { role: "user", content: prompt }],
       return_intermediate_steps: true,
     });
   } catch (error) {
